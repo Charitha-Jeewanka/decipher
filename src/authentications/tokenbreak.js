78abc2365518a0e1a48cc75d9a46cdaad4020534
Charitha-Jeewanka <cjrathnayake98@gmail.com>
2021-06-26 00:50:47 +0530

    login

--- src/authentications/tokenbreak.js
+++ src/authentications/tokenbreak.js
@@ -19,22 +19,22 @@ export const tokenBreak = () => {
     },
   });
 
-  function App() {
-    const [users, setUsers] = useState([]);
-    const [requestError, setRequestError] = useState();
-    const fetchData = useCallback(async () => {
-      try {
-        const result = await authAxios.get(`/api/users`);
-      } catch (err) {
-        setRequestError(err.message);
-      }
-    });
-    return (
-      <div>
-        {users.map((user) => {
-          return <p key={user.userID}>{user.username}</p>;
-        })}
-      </div>
-    );
-  }
+  //   function App() {
+  //     const [users, setUsers] = useState([]);
+  //     const [requestError, setRequestError] = useState();
+  //     const fetchData = useCallback(async () => {
+  //       try {
+  //         const result = await authAxios.get(`/api/users`);
+  //       } catch (err) {
+  //         setRequestError(err.message);
+  //       }
+  //     });
+  //     return (
+  //       <div>
+  //         {users.map((user) => {
+  //           return <p key={user.userID}>{user.username}</p>;
+  //         })}
+  //       </div>
+  //     );
+  //   }
 };
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,55 +1,137 @@
-import React from "react";
+import React, { Component, useState } from "react";
 import "../styles/login.css";
 import { tokenBreak } from "../authentications/tokenbreak";
+import axios from "axios";
 
 const clickHandler = () => {
   tokenBreak(1);
 };
 
-const login = () => {
-  return (
-    <section>
-      <div className="login">
-        <div className="app-title">
-          <h1>Login</h1>
-        </div>
-        <div className="login-form">
-          <div className="control-group">
-            <input
-              type="email"
-              class="login-field"
-              value=""
-              placeholder="email"
-              id="login-pass"
-            />
-            <label class="login-field-icon fui-lock" for="login-pass"></label>
-          </div>
-          <div className="control-group">
-            <input
-              type="password"
-              class="login-field"
-              value=""
-              placeholder="password"
-              id="login-pass"
-            />
-            <label class="login-field-icon fui-lock" for="login-pass"></label>
-          </div>
-          <button class="btn btn-primary btn-large btn-block" href="#">
-            Login
-          </button>
-          <button
-            class="btn btn-primary btn-large btn-block"
-            onClick={clickHandler}
-          >
-            Register
-          </button>
-          <a class="login-link" href="#">
-            Lost your password?
-          </a>
-        </div>
-      </div>
-    </section>
-  );
-};
+// const clickSender = () => {
+//   axios
+//     .post("https://ecoerest.com/", {
+//       email: "setemailName",
+//       password: "setpassWord",
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// };
 
-export default login;
+// const Login = () => {
+//   const [emailName, setemailName] = useState("");
+//   const [passWord, setpassWord] = useState("");
+//   changeHandler = e => {
+//       this.setState({[e.target.name]: e.target.value})
+//   }
+//   return (
+//     <>
+//       <article>
+//         <h2>Login</h2>
+//         <div className="login-form">
+//           <div className="form-control">
+//             <label>Email </label>
+//             <input
+//               type="email"
+//               id="emailId"
+//               placeholder="Enter Email"
+//               name="emailName"
+//               value={emailName}
+//               onChange={(e) => setemailName(e.target.value)}
+//             />
+//           </div>
+//           <div className="form-control">
+//             <label>Password </label>
+//             <input
+//               type="password"
+//               id="passId"
+//               placeholder="Enter Password"
+//               name="passName"
+//               value={passWord}
+//               onChange={(e) => setpassWord(e.target.value)}
+//             />
+//           </div>
+//           <button onClick={clickSender} className="btn">
+//             Login
+//           </button>
+//           <button onClick={clickHandler} className="btn">
+//             Register
+//           </button>
+//         </div>
+//       </article>
+//     </>
+//   );
+// };
+
+// export default Login;
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailName: "",
+      passName: "",
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    axios
+      .post("https://ecoerest.com", this.state)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  render() {
+    const { emailName, passName } = this.state;
+    return (
+      <>
+        <article>
+          <h2>Login</h2>
+          <div className="login-form">
+            <div className="form-control">
+              <label>Email </label>
+              <input
+                type="email"
+                id="emailId"
+                placeholder="Enter Email"
+                name="emailName"
+                value={emailName}
+                onChange={this.changeHandler}
+              />
+            </div>
+            <div className="form-control">
+              <label>Password </label>
+              <input
+                type="password"
+                id="passId"
+                placeholder="Enter Password"
+                name="passName"
+                value={passName}
+                onChange={this.changeHandler}
+              />
+            </div>
+            <button className="btn" onClick={this.submitHandler}>
+              Login
+            </button>
+            <button onClick={clickHandler} className="btn">
+              Register
+            </button>
+          </div>
+        </article>
+      </>
+    );
+  }
+}
+
+export default PostForm;
